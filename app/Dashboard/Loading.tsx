c730b1ddeded9602983f47271c3b891ebe00f746
KenObul <obulken10@gmail.com>
2025-09-22 04:34:41 +0300

    hello

---
+++ app/Dashboard/Loading.tsx
@@ -0,0 +1,10 @@
+// components/Loading.tsx
+// app/Dashboard/loading.tsx
+
+export default function DashboardLoading() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-900">
+      <p className="text-white text-xl">Loading dashboard content...</p>
+    </div>
+  );
+}
